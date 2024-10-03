ce3c1d7603d3cacfcd76d20cffd3ccec31709b61
vinz86 <vincenzo.disalvatore@gmail.com>
2024-10-03 18:01:36 +0200

    Avanzamento

--- builder/BuildContextMenu/Implementations/EditorContextMenu.ts
+++
@@ -1,7 +0,0 @@
-import type { IContextMenu } from '~/models/interfaces/IContextMenu';
-
-export class ContextMenuBuilder {
-    public static build<T extends IContextMenu>(ContextMenuClass: new (handlers: any) => T, handlers: any): T {
-        return new ContextMenuClass(handlers);
-    }
-}
